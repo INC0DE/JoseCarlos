6b54d9269a25dc7d82ddbe7349778f19c50d699c
Carlos Pacheco Aviles <carlospachecoaviles@192.168.68.106>
2025-02-20 01:13:10 -0600

    Domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://INC0DE.github.io/JoseCarlos/"
+  base:"https://www.josecarloscervantes.com"
 })
