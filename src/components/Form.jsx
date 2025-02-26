938ed7c6007ce217b92a7fbfd94027d7db4627e3
Carlos Pacheco Aviles <carlospachecoaviles@MacBook-Pro-de-Carlos.local>
2025-02-26 17:54:00 -0600

    Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
-import { Form, Input, Button,  Textarea  } from "@heroui/react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Form, Input, Button, Textarea } from "@heroui/react";
 
 const FormContact = () => {
-  const [action, setAction] = useState(null);
+  const form = useRef();
+  const [success, setSuccess] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_eshq1mq", "template_i8nv48k", form.current, {
+        publicKey: "uViG1V13b8DoGq2Qw",
+      })
+      .then(
+        () => {
+          setSuccess("SUCCESS!");
+          form.current.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div className="w-full">
       <Form
         className="flex flex-col gap-10"
+        ref={form}
+        onSubmit={sendEmail}
         validationBehavior="native"
-        onReset={() => setAction("reset")}
-        onSubmit={(e) => {
-          e.preventDefault();
-          let data = Object.fromEntries(new FormData(e.currentTarget));
-
-          setAction(`submit ${JSON.stringify(data)}`);
-        }}
       >
         <Input
           isRequired
@@ -53,24 +68,24 @@ const FormContact = () => {
           size="lg"
           errorMessage="Please enter a valid email"
           label="Lugar, descripción."
-          name="email"
+          name="description"
           placeholder="Describe el evento."
           type="email"
         />
         <div className="flex gap-2 w-full justify-center">
-          <Button color="primary" type="submit" size="lg">
+          <Button color="primary" type="submit">
             Subir
           </Button>
-          <Button type="reset" variant="flat" size="lg">
+          <Button type="reset" variant="flat" value="Send">
             Cancelar
           </Button>
         </div>
-        {action && (
-          <div className="text-small text-default-500">
-            Action: <code>{action}</code>
-          </div>
-        )}
       </Form>
+      {success && (
+        <p className="text-green justify-self-center mt-5">
+          ¡Formulario enviado con éxito!
+        </p>
+      )}
     </div>
   );
 };
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -6,7 +6,7 @@ const Socials = () => {
       <a
         href="https://www.instagram.com/josecarloscervantesmx/"
         aria-label="Instagram de Jose Carlos Cervantes"
-        className="flex items-center gap-4 w-64 h-52 bg-black p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72"
+        className="flex items-center gap-4 w-64 h-52 bg-black border-2 border-white p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72"
       >
         <FaInstagram className="w-16 h-16 text-[#e95950]" />
         <h1 className="text-3xl font-bold text-gray group-hover:text-black">
@@ -17,18 +17,18 @@ const Socials = () => {
       <a
         href="https://www.facebook.com/JoseCarlosCervantesCantanteRanchero"
         aria-label="Facebook de Jose Carlos Cervantes"
-        className="flex items-center gap-4 w-64 h-52 bg-black p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72 "
+        className="flex items-center gap-4 w-64 h-52 bg-black border-2 border-white p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72 "
       >
         <FaFacebook className="w-16 h-16 text-blue" />
-        <h3 className="text-3xl font-bold text-gray group-hover:text-black">
+        <h1 className="text-3xl font-bold text-gray group-hover:text-black">
           Facebook
-        </h3>
+        </h1>
       </a>
 
       <a
         href="https://www.tiktok.com/@josecarloscervantesmx"
         aria-label="TikTok de Jose Carlos Cervantes"
-        className="flex items-center gap-4 w-64 h-52 bg-black p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72"
+        className="flex items-center  gap-4 w-64 h-52 bg-black border-2 border-white p-6 rounded-lg shadow-lg transition-all duration-300 hover:bg-white hover:scale-105 lg:w-72"
       >
         <FaTiktok className="w-16 h-16 text-[#ff0050]" />
         <h3 className="text-3xl font-bold text-gray group-hover:text-black">
